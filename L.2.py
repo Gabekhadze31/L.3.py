3a00125c8cdbeff4810493ed5e87240e7e4ad03b
Giorgi Gabekhadze <g.gabekhadze@buckswood.ge>
2024-11-09 16:20:52 +0400

    L.3

---
+++ L.2.py
@@ -0,0 +1,4 @@
+ibnamesur = str(input("Please enter your name and surname: "))
+ibage = str(input("Please enter your age: "))
+ibfull = "Welcome " + ibnamesur + ". you are " + ibage + " years old!"
+print(ibfull)
---
+++ L.3.py
@@ -0,0 +1,48 @@
+# whatactionyouwannado = str(input("enter what action you want to do? sum; minus; devide; multiplication : "))
+
+# if whatactionyouwannado == "sum":
+
+
+#     sumfd = int(input("enter first digit for sum: "))
+#     sumsd = int(input("enter second digit for sum: "))
+#     print(sumfd + sumsd)
+
+# if whatactionyouwannado == "minus":
+
+#     minusfd =int(input("enter first digit for minus: "))
+#     minussd =int(input("enter first digit for minus: "))
+#     print(minusfd - minussd)
+
+# if whatactionyouwannado == "devide":
+
+#     devidefd =int(input("enter first digit for devide: "))
+#     devidesd =int(input("enter second digit for devide:"))
+#     print(devidefd / devidesd)
+
+# if whatactionyouwannado == "multiplication":
+
+#     multifd =int(input("enter first digit for multiplication: "))
+#     multisd =int(input("enter first digit for multiplication: "))
+#     print(multifd * multisd)
+
+# name = str(input("enter your name:"))
+# surname = str(input("enter your surname:"))
+# age = int(input("enter your age:"))
+
+# if name == "giorgi":
+#     print("nice")
+
+# else: print("bad")
+    
+
+# if surname == "gabekhadze":
+#     print("nice")
+    
+# else: print("bad")
+    
+
+# if age == "13":
+#     print("nice")
+
+# else: print("bad")
+    
